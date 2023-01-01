e5f41d5127d611fe41881362660c00fd76f5aab9
Luis Tellez <luis@luistellez.com>
2022-12-31 17:12:07 -0800

    feat: add user controller as well as divide error messages in separate files

---
+++ constants/ErrorMessages.js
@@ -0,0 +1,14 @@
+module.exports = {
+  ITEM_NOT_FOUND: "El recurso solicitado no se ha encontrado",
+  BAD_REQUEST: "No se permite tal entrada",
+  DB_CONFLICT: "El usuario con esta dirección ya está registrado",
+  BAD_ID: "Formato de ID no válido",
+  BAD_EMAIL: "Por favor ingrese una dirección de correo electrónico válida",
+  BAD_NAME: "Este nombre no está permitido, use letras y números en inglés",
+  BAD_PASS:
+    "La contraseña debe contener al menos 6 caracteres, una letra mayúscula y minúscula, un número y un carácter especial",
+  SHORT_PASS: "La contraseña es demasiado corta",
+  BAD_URL: "Formato de URL no válido",
+  BAD_LOGIN: "Nombre de usuario o contraseña no válidos",
+  FORBIDDEN: "No tiene permiso para realizar esta acción",
+};
