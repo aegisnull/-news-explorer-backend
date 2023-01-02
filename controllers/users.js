ad974b1b3b8a5d336112a920b0528445760ef40a
Luis Tellez <luis@luistellez.com>
2023-01-02 11:42:25 -0800

    feat: add bcrypt to hash passwords when creating users

--- controllers/users.js
+++ controllers/users.js
@@ -1,27 +1,32 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const error = require('../helpers/Errors');
 const message = require('../constants/ErrorMessages');
 
 module.exports.createUser = (req, res, next) => {
   const { email, password, name } = req.body;
-  User.create({ email, password, name })
+  bcrypt
+    .hash(password, 10)
+    .then((hash) =>
+      User.create({
+        email,
+        password: hash,
+        name
+      })
+    )
     .then(() => {
       res.status(201).send({ name, email });
     })
     .catch((err) => {
       if (err.code === 11000) {
         next(new error.DBconflict(message.DB_CONFLICT));
-      } else if (
-        err.message
-        === 'user validation failed: name: Formato de nombre no válido'
-      ) {
+      } else if (err.message === 'user validation failed: name: Formato de nombre no válido') {
         next(new error.BadRequest(message.BAD_NAME));
-      } else if (
-        err.message
-        === 'user validation failed: email: Formato de nombre no válido'
-      ) {
+      } else if (err.message === 'user validation failed: email: Formato de nombre no válido') {
         next(new error.BadRequest(message.BAD_EMAIL));
       } else {
         next(new error.BadRequest(message.SHORT_PASS));
@@ -52,16 +57,12 @@ module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
-        expiresIn: '7d',
-      });
-      const { name } = user;
-      res
-        .cookie('jwt', token, {
-          maxAge: 3600000 * 24 * 7,
-          httpOnly: true,
-        })
-        .send({ name, token });
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+        { expiresIn: '7d' }
+      );
+      res.send({ ...user.toJSON(), token });
     })
     .catch(() => next(new error.Unauthorized(message.BAD_LOGIN)));
 };
