6c22c92818fe3cdf2d6f28b68b58f2bcc247c845
Luis Tellez <luis@luistellez.com>
2023-01-02 22:45:48 -0800

    fix: fix login backend

--- controllers/users.js
+++ controllers/users.js
@@ -8,11 +8,7 @@ const message = require('../constants/ErrorMessages');
 
 module.exports.createUser = (req, res, next) => {
   const { email, password, name } = req.body;
-  User.create({
-    email,
-    password,
-    name
-  })
+  User.create({ email, password, name })
     .then(() => {
       res.status(201).send({ name, email });
     })
@@ -50,12 +46,14 @@ module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign(
-        { _id: user._id },
-        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
-        { expiresIn: '7d' }
-      );
-      res.send({ ...user.toJSON(), token });
+      const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '7d' });
+      const { name } = user;
+      res
+        .cookie('jwt', token, {
+          maxAge: 3600000 * 24 * 7,
+          httpOnly: true
+        })
+        .send({ name, token });
     })
     .catch(() => next(new error.Unauthorized(message.BAD_LOGIN)));
 };
